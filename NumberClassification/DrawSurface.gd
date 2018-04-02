76da95c2e755d7ddd2fcf0bace876a00c776af66
Niklas Andersson <me@niklasandersson.net>
2018-04-02 15:07:54 +0200

    Add clear button

--- NumberClassification/DrawSurface.gd
+++ NumberClassification/DrawSurface.gd
@@ -3,19 +3,20 @@ extends Control
 var lines = []
 var current_line = -1
 var pressed = false
+var viewport
 
 func _ready():
-	pass
+	viewport = get_viewport()
 
 func _draw():
 	for i in range(current_line + 1):
 		if lines[i].size() > 2:
-			draw_polyline(lines[i], Color(0.2, 1.0, .7, 1), 10, true)
+			draw_polyline(lines[i], Color(0, 0, 0, 1), 10, true)
 
 func _process(delta):
 	update()
 
-func _input(event):
+func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			current_line += 1
@@ -25,4 +26,14 @@ func _input(event):
 			pressed = false
 	elif event is InputEventMouseMotion:
 		if pressed:
-			lines[current_line].push_back(event.position)
+			if event.position.x < get_size().x && event.position.y < get_size().y:
+				lines[current_line].push_back(event.position)
+
+
+func _on_ClearButton_pressed():
+	lines.clear()
+	current_line = -1
+
+
+func _on_IdentifyButton_pressed():
+	pass
